a778a9daed357e0401ff5691e08298bd21272641
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-06-24 16:11:05 +0200

    move loop

--- pytorch_lightning/trainer/properties.py
+++ pytorch_lightning/trainer/properties.py
@@ -491,7 +491,7 @@ class TrainerProperties(ABC):
     @property
     def evaluation_loop(self) -> EvaluationLoop:
         if self.state.fn in (TrainerFn.FITTING, TrainerFn.TUNING):
-            return self.fit_loop.val_loop
+            return self.fit_loop.epoch_loop.val_loop
         elif self.state.fn == TrainerFn.VALIDATING:
             return self.validation_loop
         elif self.state.fn == TrainerFn.TESTING:
